1919ab55715336aef1dfba770a802226979c7afe
tddigitallibrary <110930907+tddigitallibrary@users.noreply.github.com>
2026-03-16 14:33:51 +0800

    Update register.js

--- register.js
+++ register.js
@@ -9,17 +9,20 @@ doc,
 setDoc
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
+
 window.register = async function(){
 
+try{
+
 let nama = document.getElementById("nama").value;
-
 let email = document.getElementById("email").value;
-
 let password = document.getElementById("password").value;
 
-let user = await createUserWithEmailAndPassword(auth,email,password);
+let userCredential = await createUserWithEmailAndPassword(auth,email,password);
 
-await setDoc(doc(db,"users",user.user.uid),{
+let uid = userCredential.user.uid;
+
+await setDoc(doc(db,"users",uid),{
 
 nama:nama,
 email:email,
@@ -30,5 +33,11 @@ role:"user"
 alert("Registrasi berhasil");
 
 window.location="index.html";
+
+}catch(error){
+
+alert(error.message);
+
+}
 
 }

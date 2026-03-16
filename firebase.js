c0e9fc19c395248de9172e87516067dc036d7233
tddigitallibrary <110930907+tddigitallibrary@users.noreply.github.com>
2026-03-16 13:11:50 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,25 +1,34 @@
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 
-import { getAuth }
-from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
-import { getFirestore }
-from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
 
+// CONFIG FIREBASE PROJECT ANDA
 const firebaseConfig = {
-    apiKey: "AIzaSyBxO6-KZYaWRLnGuO2bhQ0oP_Rs0xjppoE",
-    authDomain: "peminjaman-buku-77482.firebaseapp.com",
-    projectId: "peminjaman-buku-77482",
-    storageBucket: "peminjaman-buku-77482.firebasestorage.app",
-    messagingSenderId: "189272980506",
-    appId: "1:189272980506:web:90486cc16aa7fb814165a3",
-    measurementId: "G-VPXLXPQJ1D"
-  };
+  apiKey: "AIzaSyBxO6-KZYaWRLnGuO2bhQ0oP_Rs0xjppoE",
+  authDomain: "peminjaman-buku-77482.firebaseapp.com",
+  projectId: "peminjaman-buku-77482",
+  storageBucket: "peminjaman-buku-77482.firebasestorage.app",
+  messagingSenderId: "189272980506",
+  appId: "1:189272980506:web:90486cc16aa7fb814165a3",
+  measurementId: "G-VPXLXPQJ1D"
+};
 
+
+// INISIALISASI FIREBASE
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
 
-export const db = getFirestore(app);
+// AUTHENTICATION
+const auth = getAuth(app);
+
+
+// DATABASE FIRESTORE
+const db = getFirestore(app);
+
+
+// EXPORT AGAR BISA DIPAKAI FILE LAIN
+export { auth, db };

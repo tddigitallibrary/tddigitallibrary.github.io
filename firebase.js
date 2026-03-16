5a273fc86f80f97cc11c400ba9381b2c24b67f7a
tddigitallibrary <110930907+tddigitallibrary@users.noreply.github.com>
2026-03-16 13:04:27 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,25 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBxO6-KZYaWRLnGuO2bhQ0oP_Rs0xjppoE",
-  authDomain: "peminjaman-buku-77482.firebaseapp.com",
-  projectId: "peminjaman-buku-77482",
-  storageBucket: "peminjaman-buku-77482.firebasestorage.app",
-  messagingSenderId: "189272980506",
-  appId: "1:189272980506:web:90486cc16aa7fb814165a3",
-  measurementId: "G-VPXLXPQJ1D"
-};
+    apiKey: "AIzaSyBxO6-KZYaWRLnGuO2bhQ0oP_Rs0xjppoE",
+    authDomain: "peminjaman-buku-77482.firebaseapp.com",
+    projectId: "peminjaman-buku-77482",
+    storageBucket: "peminjaman-buku-77482.firebasestorage.app",
+    messagingSenderId: "189272980506",
+    appId: "1:189272980506:web:90486cc16aa7fb814165a3",
+    measurementId: "G-VPXLXPQJ1D"
+  };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);

ec1188f71beec1073e9b22c584e65116e96458f2
tddigitallibrary <110930907+tddigitallibrary@users.noreply.github.com>
2026-03-17 09:30:04 +0800

    Update user.js

--- user.js
+++ user.js
@@ -2,97 +2,27 @@ import { db } from "./firebase.js";
 
 import {
 collection,
-getDocs,
-addDoc
+getDocs
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
 const tabelBuku = document.getElementById("tabelBuku");
 const tabelPinjam = document.getElementById("tabelPinjam");
-const searchInput = document.getElementById("search");
 
-let dataBuku=[];
+async function loadDashboard(){
 
+/* TOTAL BUKU */
 
-async function loadBuku(){
+const bukuSnapshot = await getDocs(collection(db,"buku"));
 
-const snapshot = await getDocs(collection(db,"buku"));
-
-console.log("jumlah buku:",snapshot.size);
-
-snapshot.forEach(doc=>{
-console.log(doc.data());
-});
-
-}
-
-loadBuku();
+document.getElementById("totalBuku").innerText = bukuSnapshot.size;
 
 
-window.pinjamBuku = async function(judul){
+/* TOTAL PINJAM */
 
-await addDoc(collection(db,"peminjaman"),{
+const pinjamSnapshot = await getDocs(collection(db,"peminjaman"));
 
-judul,
-status:"dipinjam"
-
-});
-
-alert("Buku berhasil dipinjam");
-
-loadPinjaman();
+document.getElementById("totalPinjam").innerText = pinjamSnapshot.size;
 
 }
 
-
-async function loadPinjaman(){
-
-tabelPinjam.innerHTML="";
-
-const snapshot = await getDocs(collection(db,"peminjaman"));
-
-document.getElementById("totalPinjam").innerText = snapshot.size;
-
-snapshot.forEach(doc=>{
-
-const pinjam = doc.data();
-
-tabelPinjam.innerHTML += `
-<tr>
-<td>${pinjam.judul}</td>
-<td>${pinjam.status}</td>
-</tr>
-`;
-
-});
-
-}
-
-loadPinjaman();
-
-
-/* SEARCH BUKU */
-
-searchInput.addEventListener("keyup",()=>{
-
-const keyword = searchInput.value.toLowerCase();
-
-tabelBuku.innerHTML="";
-
-dataBuku
-.filter(b => b.judul.toLowerCase().includes(keyword))
-.forEach(b=>{
-
-tabelBuku.innerHTML += `
-<tr>
-<td>${b.judul}</td>
-<td>${b.penulis}</td>
-<td>${b.stok}</td>
-<td>
-<button onclick="pinjamBuku('${b.judul}')">Pinjam</button>
-</td>
-</tr>
-`;
-
-});
-
-});
+loadDashboard();
